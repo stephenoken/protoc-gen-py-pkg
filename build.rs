26075a060ef29342a06ed57fb956549eeb1013c3
Stephen O'Kennedy <stephen.okennedy@cellpointmobile.com>
2025-06-12 16:18:07 +0200

    Added basic option for python packages

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
 use std::{fmt::Error, fs::create_dir_all, path::Path};
 
 fn main() -> Result<(), Error> {
-    let proto_dir = "protos";
+    let proto_dir = ".";
     let proto_file = Path::new(proto_dir).join("py_package.proto");
-    let dest_path = Path::new("./src/gen");
+    let dest_path = Path::new("./src/protos");
     create_dir_all(dest_path).unwrap();
 
     println!("cargo:rerun-if-changed={}", proto_dir);
@@ -14,9 +14,8 @@ fn main() -> Result<(), Error> {
         .out_dir(dest_path)
         .includes(&[proto_dir])
         .input(proto_file.display().to_string())
-        // .cargo_out_dir(&dest_path.display().to_string())
         .run_from_script();
-    println!("cargo:rerun-if-changed=src/gen");
+    println!("cargo:rerun-if-changed=src/protos");
 
     Ok(())
 }
--- examples/output/examples/v1/foo_pb2.py
+++ examples/output/examples/v1/foo_pb2.py
@@ -26,14 +26,14 @@ from google.protobuf import descriptor_pb2 as google_dot_protobuf_dot_descriptor
 import py_package_pb2 as py__package__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x15\x65xamples/v1/foo.proto\x1a google/protobuf/descriptor.proto\x1a\x10py_package.proto\"?\n\x03\x46oo\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x15\n\x03\x62\x61r\x18\x02 \x01(\x0b\x32\x08.Foo.Bar\x1a\x13\n\x03\x42\x61r\x12\x0c\n\x04name\x18\x01 \x01(\t\"\x13\n\x03\x42\x61r\x12\x0c\n\x04name\x18\x01 \x01(\tB\x0f\x82\xb5\x18\x0b\x65xamples.v1b\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x15\x65xamples/v1/foo.proto\x1a google/protobuf/descriptor.proto\x1a\x10py_package.proto\"?\n\x03\x46oo\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x15\n\x03\x62\x61r\x18\x02 \x01(\x0b\x32\x08.Foo.Bar\x1a\x13\n\x03\x42\x61r\x12\x0c\n\x04name\x18\x01 \x01(\t\"\x13\n\x03\x42\x61r\x12\x0c\n\x04name\x18\x01 \x01(\tB\x10\xa2Q\r\n\x0b\x65xamples.v1b\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'examples.v1.foo_pb2', _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
   _globals['DESCRIPTOR']._loaded_options = None
-  _globals['DESCRIPTOR']._serialized_options = b'\202\265\030\013examples.v1'
+  _globals['DESCRIPTOR']._serialized_options = b'\242Q\r\n\013examples.v1'
   _globals['_FOO']._serialized_start=77
   _globals['_FOO']._serialized_end=140
   _globals['_FOO_BAR']._serialized_start=121
