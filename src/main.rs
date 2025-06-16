06aeca71bf2d4c69beb0e0ce667e3e2d6a2db803
Stephen O'Kennedy <stephen.okennedy@cellpointmobile.com>
2025-06-16 14:32:38 +0200

    Added iterator to generate __init__ files

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,15 @@ use protobuf::{
     Message,
     plugin::{CodeGeneratorRequest, CodeGeneratorResponse},
 };
-use std::io::{BufReader, Read, Write};
-pub mod protos;
-use protos::py_package;
+use std::{io::{BufReader, Read, Write}, iter, path::Iter};
+// pub mod protos;
+use protoc_gen_py_pkg::protos::py_package;
+
+// use protoc_gen_py_pkg::protos::py_package::exts::py_package_opts;
 
 const CODE_GENERATOR_RESPONSE_FEATURE_PROTO3_OPTIONAL: u64 = 1;
+
+
 fn main() {
     env_logger::init();
     log::info!("Starting the application...");
@@ -24,52 +28,24 @@ fn main() {
     let mut response = CodeGeneratorResponse::new();
     response.set_supported_features(CODE_GENERATOR_RESPONSE_FEATURE_PROTO3_OPTIONAL);
 
-    let py_package_files = request
+    let opts: Vec<Option<py_package::PyPackageOptions>> = request
         .proto_file
         .iter()
         .map(|file| {
             let opts = py_package::exts::py_package_opts.get(&file.options);
-            if let Some(o) = opts {
+            if let Some(opt) = &opts {
                 log::info!("Found py_package options in file: {}", file.name());
-                log::info!("Options: {:?}", o);
+                log::info!("Options: {:?}", opt);
             };
-            file
+            opts
         })
-        .collect::<Vec<_>>();
+        .collect();
 
-    // py_package_files.iter().for_each(|file| {
-    //     log::info!("Found py_package file: {}", file.name());
-    // });
-
-    // request.proto_file.iter().for_each(|file| {
-    //     log::info!("Found proto file: {}", file.name());
-    //     log::info!("Options: {:?}", file.options.special_fields());
-    //     py_package::file_descriptor().extensions().for_each(|ext| {
-    //         if let Some(value) = file
-    //             .options
-    //             .special_fields()
-    //             .unknown_fields()
-    //             .iter()
-    //             .find(|v| v.0 == ext.number() as u32)
-    //         {
-    //             log::info!("Extension {}: {:?}", ext.name(), value);
-    //         } else {
-    //             log::info!("Extension {} not set", ext.name());
-    //         }
-    //     });
-    //     file.message_type.iter().for_each(|message| {
-    //         log::info!("Message: {}", message.name());
-    //     });
-    // });
-    // request.proto_file.iter().for_each(|file| {
-    //     let file_name = file.name.as_deref().unwrap_or("unknown.proto");
-    //     log::info!("Processing file: {}", file_name);
-
-    //     let mut new_file = protobuf::plugin::code_generator_response::File::new();
-    //     new_file.set_name(file_name.to_string());
-    //     new_file.set_content(format!("// Generated code for: {}\n", file_name));
-    //     response.file.push(new_file);
-    // });
+    opts.iter().flat_map(protoc_gen_py_pkg::generate_py_init_files).for_each(|file| {
+        log::info!("Generated file: {}", file.name());
+        response.file.push(file);
+    });
+    
     let output = response.write_to_bytes().unwrap();
     std::io::stdout().write_all(&output).unwrap()
 }
